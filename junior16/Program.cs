6ea314fc2e3b2371d3713447ece333966e5988f2
Danila <asd@mail.ru>
2023-04-16 14:05:38 +0300

    Добавьте файлы проекта.

---
+++ junior16/Program.cs
@@ -0,0 +1,22 @@
+﻿int minCount = 100;
+int maxCount = 999;
+int minRandom = 1;
+int maxRandom = 28;
+
+int totalCount = 0;
+
+Random rand = new Random();
+
+int number = rand.Next(minRandom, maxRandom);
+
+Console.WriteLine("N = " + number);
+
+for (int i = minCount; i < maxCount; i += number)
+{
+    if (i >= minCount)
+    {
+        totalCount++;
+    }
+}
+
+Console.WriteLine($"Количество трехзначных натуральных чисел, которые кратны N: {totalCount}");
